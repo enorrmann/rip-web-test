efc01a0953b8e72568473bd14967623342881d60
enorrmann <enorrmann@gmail.com>
2015-05-27 14:54:16 -0300

    refactor

--- src/main/java/ar/gov/santafe/meduc/rip/Metodo.java
+++
@@ -1,5 +0,0 @@
-package ar.gov.santafe.meduc.rip;
-
-public enum Metodo {
-	GET, POST, DELETE
-}
--- src/main/java/ar/gov/santafe/meduc/rip/Service.java
+++
@@ -1,9 +0,0 @@
-package ar.gov.santafe.meduc.rip;
-
-public class Service<T> {
-	public final T call;
-
-	public Service(T call) {
-		this.call = call;
-	}
-}
