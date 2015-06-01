49b32821d226819125cb49a6112cf2578e35e7d5
enorrmann <enorrmann@gmail.com>
2015-06-01 11:23:35 -0300

    using WebResourceFactory

--- src/main/java/ar/gov/santafe/meduc/lce/ui/util/controller/ProductorUtil.java
+++ src/main/java/ar/gov/santafe/meduc/lce/ui/util/controller/ProductorUtil.java
@@ -5,26 +5,31 @@ import java.lang.reflect.ParameterizedType;
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 
-import ar.gov.santafe.meduc.rip.RestInvocationHandler;
-import ar.gov.santafe.meduc.rip.Service;
+import org.glassfish.jersey.client.proxy.WebResourceFactory;
 
 
 public class ProductorUtil {
 
-	public <T> T getProxyFor(Class<T> t) {
-
-		@SuppressWarnings("unchecked")
-		T implementing = (T) java.lang.reflect.Proxy.newProxyInstance(
-				t.getClassLoader(), new java.lang.Class[] { t },
-				new RestInvocationHandler());
-		return implementing; 
-	}
+//	public <T> T getProxyFor(Class<T> t) {
+//
+//		@SuppressWarnings("unchecked")
+//		T implementing = (T) java.lang.reflect.Proxy.newProxyInstance(
+//				t.getClassLoader(), new java.lang.Class[] { t },
+//				new RestInvocationHandler());
+//		return implementing; 
+//	}
 	
 	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Produces
     public <T> Service<T> create(InjectionPoint p){ 
 		 Class clazz = (Class)((ParameterizedType)p.getType()).getActualTypeArguments()[0];
-		return new Service(getProxyFor(clazz));
+		  WebTarget t = ClientBuilder.newClient().target("http://localhost:8080/lce-persistence/");
+//		return new Service(getProxyFor(clazz));
+		return new Service(WebResourceFactory.newResource(clazz, t));
+		//  return null;
     }
 }
