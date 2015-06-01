a4e4457042c4c056a06d5f2a060d86e08b5de783
enorrmann <enorrmann@gmail.com>
2015-06-01 11:24:05 -0300

    prevent  interfaces from republishing

--- src/main/java/ar/gov/santafe/meduc/lce/ui/util/controller/App.java
+++ src/main/java/ar/gov/santafe/meduc/lce/ui/util/controller/App.java
@@ -1,9 +1,12 @@
 package ar.gov.santafe.meduc.lce.ui.util.controller;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-	
-@ApplicationPath("/")
-public class App extends Application {
 
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("/")
+public class App extends ResourceConfig {
+	public App() {
+		packages("ar.gov.santafe.meduc.lce.ui.util.controller");
+	}
 }
